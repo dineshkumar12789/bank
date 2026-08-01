1d11a3e083868b8d898204c620a995bf3527a202
dineshkumar12789 <kdinesh20395@gmail.com>
2026-08-01 23:23:51 +0530

    file updated

--- BankAccount.java
+++ BankAccount.java
@@ -1,12 +1,46 @@
 class BankAccount{
-    int accountNumber;
-    String accountName;
-    double balance;
+    private int accountNumber;
+    private String accountName;
+    private double balance;
+    
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
 
     void displayDetails(){
-        System.out.println("The accound number is:"+ accountNumber);
-        System.out.println("The accound name is:"+ accountName);
-        System.out.println("Balance amount is:"+ balance);
+        System.out.println("-----ACCOUND NUMBER-----");
+        System.out.println(getAccountNumber());
+        System.out.println("-----ACCOUND NAME-----");
+        System.out.println(getAccountNumber());
+        System.out.println("-----BALANCE-----");
+        System.out.println(getBalance());
     }
 
 }
--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         BankAccount account=new BankAccount();
-        account.accountNumber=sc.nextInt();
+        account.setAccountNumber(sc.nextInt());
         sc.nextLine();
-        account.accountName=sc.nextLine();
-        account.balance=sc.nextDouble();
+        account.setAccountName(sc.nextLine());
+        account.setBalance(sc.nextDouble());
 
         account.displayDetails();
          sc.close();
